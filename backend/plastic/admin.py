80acc31b3682b459bd0d857d4dd370bdfded9bb3
Wai Ian Vong <valenvwi@gmail.com>
2023-11-24 20:24:20 +0100

    Add Plastic and Plastic_stock model

---
+++ backend/plastic/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import Plastic, Plastic_stock
+admin.site.register(Plastic)
+admin.site.register(Plastic_stock)
---
+++ backend/plastic/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PlasticConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'plastic'
